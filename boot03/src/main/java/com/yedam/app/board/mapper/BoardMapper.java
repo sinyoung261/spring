0718502842543e6f6155e0c35dda3b7265d5883c
sinyoung261 <waterfired@naver.com>
2025-02-20 18:06:42 +0900

    250220

---
+++ boot03/src/main/java/com/yedam/app/board/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package com.yedam.app.board.mapper;
+
+import java.util.List;
+
+import com.yedam.app.board.service.BoardVO;
+
+public interface BoardMapper {
+	//전체조회
+	public List<BoardVO> selectAllList();
+	
+	//단건조회
+	public BoardVO selectInfo(BoardVO boardVO);
+	
+	//등록
+	public int insertInfo(BoardVO boardVO);
+	
+}
